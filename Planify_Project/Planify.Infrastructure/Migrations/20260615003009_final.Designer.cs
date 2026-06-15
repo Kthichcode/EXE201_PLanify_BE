0962ca2bda98642dd68ea5e3f8a190bc0b8c9b28
Kthichcode <ongquocdat2018@gmail.com>
2026-06-15 07:35:27 +0700

    Update Migration

--- Planify_Project/Planify.Infrastructure/Migrations/20260615003009_final.Designer.cs
+++ Planify_Project/Planify.Infrastructure/Migrations/20260615003009_final.Designer.cs
@@ -12,8 +12,8 @@ using Planify.Infrastructure.Data;
 namespace Planify.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260610031744_hehe")]
-    partial class hehe
+    [Migration("20260615003009_final")]
+    partial class final
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
